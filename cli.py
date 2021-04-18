1a4a5693943d66ea1189c68dba7d5fbee5dcc215
Maxwell Brown <maxwellgbrown@gmail.com>
2021-04-17 23:18:31 -0400

    Add whether moves have been acquired

--- cli.py
+++ cli.py
@@ -23,10 +23,47 @@ def _statline(value="ivs", maximum=31):
     return dict(stats)
 
 
+def _moves():
+    moves = {}
+    for num in range(1, 5):
+        move = input(f"Move {num}: ")
+        acquired = input(f"Uncheck move (y)? ").lower() != "y"
+        moves[move] = acquired
+
+    return moves
+
+
 def _default(node):
     if isinstance(node, decimal.Decimal):
         return int(node)
     return json.JSONDecoder.encode(node)
+
+
+def _pretty(item):
+    """Pretty print a record from DynamoDB."""
+    ivs = "IVs: \n"
+    for stat, value in item.get("ivs", {}).items():
+        ivs += f"  {value} {stat}\n"
+
+    evs = "EVs: \n"
+    for stat, value in item.get("evs", {}).items():
+        evs += f"  {value} {stat}\n"
+
+    moves = "\n"
+    for move, acquired in item.get("moves", {}).items():
+        moves += f"  [{'x' if acquired else ' '}] {move}\n"
+
+    return f"""
+-------------------------------------------------------------------------------
+{item['Pokemon']} - {item.get('Index')}
+{item.get('nickname', item['Pokemon'])}
+
+Ability: {item.get('ability')}
+Nature: {item.get('nature')}
+{ivs}
+{evs}
+{moves}
+"""
 
 
 class RosterCmd(cmd.Cmd):
@@ -50,20 +87,23 @@ class RosterCmd(cmd.Cmd):
         """Scan the table for pokemon entries."""
         results = self._table.scan()
         for item in results["Items"]:
-            self._print(json.dumps(item, indent=2, default=_default))
+            self._print(_pretty(item))
 
+    # TODO Find a more intiutive way to hand-enter this data
     def do_insert(self, pokemon):
         """Begin an insert for this record."""
         while input(f"Insert a new record for \"{pokemon}\" (y?): ") == "y":
             self._table.put_item(Item={
-                "Pokemon": pokemon,
+                "Pokemon": pokemon,  # TODO Validate against a list
                 "Index": _index(),
                 "nickname": input("nickname: "),
-                "ability": input("ability: "),
-                "nature": input("nature: "),
+                "ability": input("ability: "),  # TODO Validate against a list
+                "nature": input("nature: "),  # TODO Validate against a list
                 "ivs": _statline("ivs", 31),
                 "evs": _statline("evs", 255),
-                "moves": [input(f"move {i + 1}: ") for i in range(4)]
+                # TODO does order of moves matter?
+                "moves": _moves(),
+                "egg_moves": input("egg moves (,-delim): ").split(","),
             })
 
     def do_query(self, pokemon):
@@ -77,12 +117,11 @@ class RosterCmd(cmd.Cmd):
             ExpressionAttributeValues={
                 ":pokemon": pokemon,
                 ":index": "0",
-
             }
         )
 
         for item in results["Items"]:
-            self._print(json.dumps(item, indent=2, default=_default))
+            self._print(_pretty(item))
 
 
 if __name__ == "__main__":
